9ba09791294f673e1609e0d8b34f0875964168b4
Laysan <lise@Laysans-MacBook-Pro.local>
2021-02-02 20:20:06 +0800

    update

--- surgeon-mbg-plugin/src/main/java/com/github/chassis/mbg/plugins/SwaggerPlugin.java
+++ surgeon-mbg-plugin/src/main/java/com/github/chassis/mbg/plugins/SwaggerPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Laysan (lslvxy@gmail.com)
+ * Copyright © ${project.inceptionYear} Laysan (lslvxy@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
